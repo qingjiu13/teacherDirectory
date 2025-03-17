6dc15f658a822c8ccdbb3122b98e42ffd8ebef68
Zhang Teng <2351795@tongji.edu.cn>
2025-03-17 15:54:48 +0800

    Update vendor.js

--- 前端yanshilu/unpackage/dist/dev/mp-weixin/common/vendor.js
+++ 前端yanshilu/unpackage/dist/dev/mp-weixin/common/vendor.js
@@ -7564,9 +7564,9 @@ function initOnError() {
   };
 }
 function initRuntimeSocketService() {
-  const hosts = "100.81.1.13,127.0.0.1";
+  const hosts = "100.78.77.216,127.0.0.1";
   const port = "8090";
-  const id = "mp-weixin_igs9ja";
+  const id = "mp-weixin_0Zf4Bn";
   const lazy = typeof swan !== "undefined";
   let restoreError = lazy ? () => {
   } : initOnError();
