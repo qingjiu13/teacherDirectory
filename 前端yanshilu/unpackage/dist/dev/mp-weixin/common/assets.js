1b6f6013ee54caffe3c12690186a284a32546c5e
Zhang Teng <2351795@tongji.edu.cn>
2025-03-22 19:44:57 +0800

    增加了我的页面中我的帖子部分

--- 前端yanshilu/unpackage/dist/dev/mp-weixin/common/assets.js
+++ 前端yanshilu/unpackage/dist/dev/mp-weixin/common/assets.js
@@ -1,2 +1,2 @@
-"use strict";exports._imports_0="/static/image/plus-1727487.svg",exports._imports_0$1="/static/image/arrow_down.svg",exports._imports_0$2="/static/image/comment.svg",exports._imports_0$3="/static/image/default-avatar.png",exports._imports_0$4="/static/robot.png",exports._imports_1="/static/image/share.svg";
+"use strict";exports._imports_0="/static/image/plus-1727487.svg",exports._imports_0$1="/static/image/arrow_down.svg",exports._imports_0$2="/static/image/comment.svg",exports._imports_0$3="/static/image/default_avatar.png",exports._imports_0$4="/static/robot.png",exports._imports_1="/static/image/share.svg";
 //# sourceMappingURL=../../.sourcemap/mp-weixin/common/assets.js.map
