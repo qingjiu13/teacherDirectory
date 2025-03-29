4768885f60a72aa18ced8f035f21e6510f033d18
Zhang Teng <2351795@tongji.edu.cn>
2025-03-29 14:44:47 +0800

    在前端yanshilu_1里面写
    之前那个先不删，页面是可以直接用的，script逻辑可能需要修改

--- 前端yanshilu/unpackage/dist/dev/mp-weixin/utils/auth.js
+++
@@ -1,2 +0,0 @@
-"use strict";const t=require("../common/vendor.js"),r=require("./jwt.js"),e=0,n=1;exports.isLoggedIn=function(){return r.hasToken()&&function(){try{const r=t.index.getStorageSync("loginStatus");return""!==r?r:e}catch(r){return t.index.__f__("error","at utils/auth.js:72","获取登录状态失败:",r),e}}()===n};
-//# sourceMappingURL=../../.sourcemap/mp-weixin/utils/auth.js.map
---
+++ 前端yanshilu_1/router.js
@@ -0,0 +1,9 @@
+/**
+ * @description 统一路由管理
+ * 集中管理所有页面的路由路径，提供导航方法
+ */
+export const Routes = {
+	MATCH:'/pages/match/match',
+	AI_SERVER:'/pages/AI/AI',
+	
+}
